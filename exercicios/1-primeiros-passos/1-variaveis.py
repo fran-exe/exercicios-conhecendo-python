bb5c652285490493189d748d57c20a711c35c224
fran-exe <fran.x3r@gmail.com>
2024-07-18 18:45:11 +0000

    Resolução de exercício

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -4,8 +4,9 @@
 # - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
 
 # Declare variáveis do tipo string
-
+nome = "Fran"
 # Declare variáveis de cada tipo numérico
-
+idade = "28"
 
 # Declare uma variável do tipo booleano
+aprendendo_python = "true"
